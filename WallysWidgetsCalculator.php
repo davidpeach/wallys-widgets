d0d9f82b2d11642df7c369501b062554db946de3
David Peach <mail@davidpeach.co.uk>
2020-12-12 14:49:51 +0000

    1 returns 250

--- WallysWidgetsCalculator.php
+++ WallysWidgetsCalculator.php
@@ -3,6 +3,11 @@
 class WallysWidgetsCalculator
 {
     /**
+     * @var array
+     */
+    private $packSizes = [];
+
+    /**
      * Your solution should return an array with the pack sizes as the key
      * and the number of packs in that size as the value.
      *
@@ -22,6 +27,24 @@ class WallysWidgetsCalculator
      */
     public function getPacks(int $widgetsRequired, array $packSizes): array
     {
+        $this->packSizes = $packSizes;
+
+        foreach ($this->packSizes as $size) {
+            if ($this->packSizeExists($size)) {
+                return [$size => 1];
+            }
+        }
+
         return [];
+    }
+
+    /**
+     * Determine if a given pack size exists in the available pack sizes.
+     * @param  int  $size
+     * @return bool
+     */
+    private function packSizeExists(int $size): bool
+    {
+        return in_array($size, $this->packSizes);
     }
 }
